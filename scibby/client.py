f0259b507d2edcc9da510c7426a73b4248c70657
Simon de Vlieger <simon@ikanobori.jp>
2012-01-16 00:36:24 +0100

    make things work

--- scibby/client.py
+++ scibby/client.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
+import sys
+
 from twisted.internet import reactor, task, defer, protocol
 from twisted.python import log
 from twisted.words.protocols import irc
 from twisted.application import internet, service
 
-import sys
+from scibby import commands
 
 HOST = "irc.quakenet.org"
 PORT = 6667
 
-class IRCClient(irc.IRCClient):
+class ScibbyClient(irc.IRCClient):
     nickname = "scibby"
 
     def signedOn(self):
@@ -26,40 +28,39 @@ class IRCClient(irc.IRCClient):
 
         command, sep, rest = message.lstrip("!").partition(" ")
 
-        # get from seperate module later
-        func = getattr(self, "command_" + command, None)
+        func = getattr(commands, "command_" + command, None)
 
         if func is None:
             return
 
         d = defer.maybeDeferred(func, rest)
-        d.addErrback(self._show_error)
+        # d.addErrback(self._show_error)
 
         if channel == self.nickname:
             d.addCallback(self._send_message, nick)
         else:
             d.addCallback(self._send_message, channel, nick)
 
-        def _send_message(self, msg, target, nick=None):
-            if nick:
-                msg = "%s, %s" % (nick, msg)
-            self.msg(target, msg)
+    def _send_message(self, msg, target, nick=None):
+        if nick:
+            msg = "%s, %s" % (nick, msg)
+        self.msg(target, msg)
 
-        def _show_error(self, failure):
-            return failure.getErrorMessage()
+    def _show_error(self, failure):
+        return failure.getErrorMessage()
 
-        def command_ping(self, rest):
-            return "Pong."
+    def command_ping(self, rest):
+        return "Pong."
 
-class IRCFactory(protocol.ReconnectingClientFactory):
-    protocol = IRCClient
-    channels = ["#redditeu"]
+class ScibbyFactory(protocol.ReconnectingClientFactory):
+    protocol = ScibbyClient 
+    channels = ["#scibbytest"]
 
 if __name__ == "__main__":
-    reactor.connectTCP(HOST, PORT, IRCFactory())
+    reactor.connectTCP(HOST, PORT, ScibbyFactory())
     log.startLogging(sys.stdout)
     reactor.run()
 elif __name__ == "__builtin__":
     application = service.Application("scib")
-    irc_service = internet.TCPClient(HOST, PORT, IRCFactory())
+    irc_service = internet.TCPClient(HOST, PORT, ScibbyFactory())
     irc_service.setServiceParent(application)
--- scibby/commands.py
+++ scibby/commands.py
@@ -6,13 +6,15 @@ import lxml.html
 """Commands container, to be replaced with a plugin way of dealing with things
 later on the lifecycle of scibby the irc bot!"""
 
-def command_title(self, url):
+def command_title(url):
     """Use lxml to parse the <title> tag out of a given URL"""
     d = getPage(url)
 
     d.addCallback(_parse_pagetitle, url)
 
-def _parse_pagetitle(self, page_contents, url):
+    return d
+
+def _parse_pagetitle(page_contents, url):
     pagetree = lxml.html.fromstring(page_contents)
 
     title = u" ".join(pagetree.xpath("//title/text()")).strip()
@@ -20,12 +22,22 @@ def _parse_pagetitle(self, page_contents, url):
 
     return "%s -- %s" % (url, title)
 
-def command_lolelo(self, nickname, domain="euw"):
+def command_lolelo(nickname, domain="euw"):
+    print "Getting LoL ELO for %s on %s" % (nickname, domain)
     url = "http://competitive.%s.leagueoflegends.com/ladders/%s/current/rankedsolo5x5?summoner_name=%s" % (domain, domain, nickname)
+
+    d = getPage(url)
+
+    d.addCallback(_parse_lolelo, nickname, domain)
+
+    return d
     
-def _parse_lolelo(self, page_contents, url, nickname):
+def _parse_lolelo(page_contents, nickname, domain):
     pagetree = lxml.html.fromstring(page_contents)
 
-    elo = u" ".join(pagetree.xpath("//td[@class='views-field-rating']/text()")).strip()
+    elo = u" ".join(pagetree.xpath("//td[@class='views-field views-field-rating views-align-center']/text()")).strip()
+    elo = elo.encode("utf-8")
 
-    return "%s -- %s" % (nickname, elo) 
+    print elo
+
+    return "%s (%s) -- %s" % (nickname, domain, elo) 
