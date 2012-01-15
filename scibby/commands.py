be405c28b842c01c7c493d33f7ccc53d8a4ac7a7
Simon de Vlieger <simon@ikanobori.jp>
2012-01-16 00:06:00 +0100

    Add lolelo

--- scibby/commands.py
+++ scibby/commands.py
@@ -15,7 +15,17 @@ def command_title(self, url):
 def _parse_pagetitle(self, page_contents, url):
     pagetree = lxml.html.fromstring(page_contents)
 
-    title = u"" ".join(pagetree.xpath("//title/text()")).strip()
+    title = u" ".join(pagetree.xpath("//title/text()")).strip()
     title = title.encode("utf-8")
 
     return "%s -- %s" % (url, title)
+
+def command_lolelo(self, nickname, domain="euw"):
+    url = "http://competitive.%s.leagueoflegends.com/ladders/%s/current/rankedsolo5x5?summoner_name=%s" % (domain, domain, nickname)
+    
+def _parse_lolelo(self, page_contents, url, nickname):
+    pagetree = lxml.html.fromstring(page_contents)
+
+    elo = u" ".join(pagetree.xpath("//td[@class='views-field-rating']/text()")).strip()
+
+    return "%s -- %s" % (nickname, elo) 
