74d3a8e1f843c8cd2b72da52b49e182cc387cd8a
David Trail <napalm10@gmail.com>
2012-01-19 15:33:15 +0100

    Config to new file. Derp

---
+++ scibby/config.py
@@ -0,0 +1,7 @@
+configuration = {
+    "host": "irc.quakenet.org",
+    "port": 6667,
+    "nickname": "Naybby",
+    "channels": ["#scibbytest"],
+    "plugins_directory": "/home/napalm/scibby-plugins"
+}
